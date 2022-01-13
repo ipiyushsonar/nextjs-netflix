fc21498ca5d16cb1dfa9b966f9ed93abd5c021fe
Piyush Sonar <sonar0324@gmail.com>
2022-01-13 21:47:41 +0530

    moved colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [ 'Roboto', ...fontFamily.sans ]
+      },
+      colors: {
+        'netflix-red': '#e50914',
+      },
+    },
   },
   plugins: [],
 }
