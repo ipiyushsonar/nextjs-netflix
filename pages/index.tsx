0a1ca1bd32aeab57576d3aef5166a20a043e3097
Piyush Sonar <sonar0324@gmail.com>
2022-01-13 20:20:47 +0530

    added home styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,59 @@
-import Head from 'next/head'
+import Head from "next/head";
+import CSS from "csstype";
+
+const heroStyle: CSS.Properties = {
+	backgroundImage:
+		"linear-gradient(rgb(0 0 0 / 60%), rgb(0 0 0 / 60%)), url(./hero-bg.jpg)",
+};
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2"
+    style={heroStyle}>
+			<Head>
+				<title>Netflix Clone</title>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+					rel="stylesheet"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+			<main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
+				<h1 className="max-w-lg text-5xl font-bold text-white">
+					Unlimited movies, TV shows and more.
+				</h1>
+				<h2 className="mt-4 mb-8 text-2xl text-white ">
+					Watch anywhere. Cancel anytime.
+				</h2>
+				<p className="text-lg text-white">
+					Ready to watch? Enter your email to create or restart your membership.
+				</p>
+				<div className="flex mt-4">
+					<input
+						placeholder="Email address"
+						className="p-4 bg-white min-w-[400px]"
+					/>
+					<button className="flex items-center px-8 text-xl text-white bg-[#e50914]">
+						Get Started
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="w-6 h-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M9 5l7 7-7 7"
+							/>
+						</svg>
+					</button>
+				</div>
+			</main>
+		</div>
+	);
 }
