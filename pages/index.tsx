28aaf7600bec1bf71fcd1740e8968bedc00b150c
Piyush Sonar <sonar0324@gmail.com>
2022-01-13 21:46:45 +0530

    added protected routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import CSS from "csstype";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 const heroStyle: CSS.Properties = {
 	backgroundImage:
@@ -26,29 +27,32 @@ export default function Home() {
 			</Head>
 			<header className="flex justify-between w-full px-12 py-8">
 				<div>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0.238 0.034 919.406 248.488"
-						className="w-28"
-					>
-						<path
-							fill="#e50914"
-							d="M870.46 118.314l49.184 130.208c-14.495-2.07-28.982-4.663-43.733-6.999l-27.707-71.945-28.468 66.006c-13.973-2.336-27.698-3.114-41.672-4.928l49.955-113.89L782.71.034h41.937l25.362 65.22L877.194.034h42.442zM749.596.034h-38.052v225.71c12.425.779 25.362 1.292 38.052 2.841zm-70.927 223.118c-34.68-2.328-69.37-4.39-104.829-5.177V.035h38.823v181.188c22.264.514 44.52 2.32 66.006 3.355zM532.417 88.305v38.822h-53.06v88.263h-38.3V.034H549.77v38.822h-70.405v49.45h53.06zM375.82 38.856v178.605c-12.946 0-26.14 0-38.83.514V38.856h-40.122V.034H416.19v38.822zM254.94 129.19c-17.08 0-37.274 0-51.769.787v57.715c22.778-1.557 45.556-3.363 68.59-4.141v37.273l-107.412 8.548V.034h107.405v38.822H203.17v52.29c15.017 0 38.052-.778 51.768-.778v38.83zM39.831 107.447V243.08c-13.965 1.557-26.398 3.371-39.593 5.442V.034h37.017L87.724 141.11V.034h38.83V232.47c-13.717 2.336-27.698 3.114-42.45 5.177z"
-						/>
-					</svg>
+					{/* <Link href="/"> */}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0.238 0.034 919.406 248.488"
+							className="w-28"
+							cursor="pointer"
+						>
+							<path
+								fill="#e50914"
+								d="M870.46 118.314l49.184 130.208c-14.495-2.07-28.982-4.663-43.733-6.999l-27.707-71.945-28.468 66.006c-13.973-2.336-27.698-3.114-41.672-4.928l49.955-113.89L782.71.034h41.937l25.362 65.22L877.194.034h42.442zM749.596.034h-38.052v225.71c12.425.779 25.362 1.292 38.052 2.841zm-70.927 223.118c-34.68-2.328-69.37-4.39-104.829-5.177V.035h38.823v181.188c22.264.514 44.52 2.32 66.006 3.355zM532.417 88.305v38.822h-53.06v88.263h-38.3V.034H549.77v38.822h-70.405v49.45h53.06zM375.82 38.856v178.605c-12.946 0-26.14 0-38.83.514V38.856h-40.122V.034H416.19v38.822zM254.94 129.19c-17.08 0-37.274 0-51.769.787v57.715c22.778-1.557 45.556-3.363 68.59-4.141v37.273l-107.412 8.548V.034h107.405v38.822H203.17v52.29c15.017 0 38.052-.778 51.768-.778v38.83zM39.831 107.447V243.08c-13.965 1.557-26.398 3.371-39.593 5.442V.034h37.017L87.724 141.11V.034h38.83V232.47c-13.717 2.336-27.698 3.114-42.45 5.177z"
+							/>
+						</svg>
+					{/* </Link> */}
 				</div>
 				<div>
 					{!session ? (
 						<button
 							onClick={() => signIn("google")}
-							className="flex items-center bg-[#e50914] text-white text-sm px-4 py-2 rounded"
+							className="flex items-center px-4 py-2 text-sm text-white rounded bg-netflix-red"
 						>
 							Sign In
 						</button>
 					) : (
 						<button
 							onClick={() => signOut()}
-							className="flex items-center bg-[#e50914] text-white text-sm px-4 py-2 rounded"
+							className="flex items-center px-4 py-2 text-sm text-white rounded bg-netflix-red"
 						>
 							Sign Out
 						</button>
@@ -57,7 +61,7 @@ export default function Home() {
 			</header>
 
 			<main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-				<h1 className="max-w-lg text-5xl font-bold text-white">
+				<h1 className="max-w-lg text-5xl font-medium tracking-wide text-white">
 					Unlimited movies, TV shows and more.
 				</h1>
 				<h2 className="mt-4 mb-8 text-2xl text-white ">
@@ -71,7 +75,7 @@ export default function Home() {
 						placeholder="Email address"
 						className="p-4 bg-white min-w-[400px]"
 					/>
-					<button className="flex items-center px-8 text-xl text-white bg-[#e50914]">
+					<button className="flex items-center px-8 text-xl text-white bg-netflix-red">
 						Get Started
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -83,7 +87,7 @@ export default function Home() {
 							<path
 								strokeLinecap="round"
 								strokeLinejoin="round"
-								strokeWidth={2}
+								strokeWidth={3}
 								d="M9 5l7 7-7 7"
 							/>
 						</svg>
